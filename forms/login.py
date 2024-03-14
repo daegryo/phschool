0e32ed7ace06fe42f66089b56ced6f46416e5056
daegryo <daegryo@yandex.ru>
2024-03-15 02:20:10 +0300

    log in

--- forms/login.py
+++ forms/login.py
@@ -2,5 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class WelcomeForm(FlaskForm):
-    submit = SubmitField('Log In')
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -3,22 +3,55 @@ from flask import Flask, url_for, redirect, render_template
 from data import db_session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from data.users import User
-from forms.login import WelcomeForm
+from forms.login import LoginForm
 from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = WelcomeForm()
-    return render_template('base.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        user.set_password(form.password.data)
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            print('jjjjjjjj')
+            return redirect(f"/home/<{form.email.data}>")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/home/<email>')
+def home(email):
+    new_email = ''
+    for el in email:
+
+        if el != '<' and el != '>':
+            print(el)
+            new_email += el
+    print(new_email)
+
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.email == new_email).first()
+    print(type(user))
+    return render_template('home.html', user=user)
 
 
 if __name__ == '__main__':
-
+    db_session.global_init("db/blogs.db")
     app.run(port=8080, host='127.0.0.1', debug=True)
